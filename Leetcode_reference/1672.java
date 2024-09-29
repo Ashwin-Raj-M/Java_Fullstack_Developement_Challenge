f2759201195bd34b25b6cf406be39de4da2ad928
Ashwin Raj M <103539596+Ashwin-Raj-M@users.noreply.github.com>
2024-09-29 22:28:02 +0530

    Sep_09_24_Update

--- Leetcode_reference/1672.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int maximumWealth(int[][] acc) {
-        int wel = 0;
-        for(int[] arr : acc){
-            wel = Math.max(wel, Arrays.stream(arr).sum());
-        }
-        return wel;
-    }
-}
